ec9db3a655a51e1e487a0eff3fd6609921c33b41
Del aguila <85969210+Lupani9@users.noreply.github.com>
2023-05-25 14:24:11 -0500

    Update README.d

--- README.d
+++ README.d
@@ -3,9 +3,9 @@
 #EXTINF:1- Tooncast
 http://tstv.lcdn.claro.net.co/Content/hls/Live/Channel(TOONCAST)/Stream(04)/index.m3u8
 #EXTINF:1- Disney Jr
-https://stream-02.nyc.dailymotion.com/sec(ekpi6JmRB5KslkTtNqtBmlUZD2MSj8VmvvIKy1Ta_qqhW6imqUwkgeSVeX7z1o5F)/dm/3/x7x4dgx/s/live-1.m3u8#cell=lnyc
-#EXTINF:0, Boomerang
-http://tstv.lcdn.claro.net.co/Content/hls/Live/Channel(BOOMERANG)/Stream(04)/index.m3u8
+https://stream-02.nyc.dailymotion.com /sec ( ekpi6JmRB5KslkTtNqtBmlUZD2MSj8VmvvIKy1Ta_qqhW6imqUwkgeSVeX7z1o5F ) /dm/3/x7x4dgx/s/live-1.m3u8 #cell=lnyc
+#EXTINF:0, bumerán
+http://tstv.lcdn.claro.net.co/Content/hls/Live /Channel ( BOOMERANG )  /Stream (  04  ) /index.m3u8
 #EXTINF:0, Disney XD
 http://tstv.lcdn.claro.net.co/Content/hls/Live/Channel(DISNEY_XD)/Stream(03)/index.m3u8
 
